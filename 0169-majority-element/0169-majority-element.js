8357e37c9bdaa82dff8f0a92ea987a192b954a43
HJ <91491029+koreadinosaur@users.noreply.github.com>
2023-02-08 20:28:16 +0900

    Time: 58 ms (98.07%), Space: 42.8 MB (96.24%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const numsObj = {}
+    /*const numsObj = {}
     const numsArray = [];
     nums.map((item)=>{
         numsObj[item] ? numsObj[item]++ : numsObj[item] = 1;
@@ -12,5 +12,16 @@ var majorityElement = function(nums) {
         numsArray.push([property,numsObj[property]])
     };
     numsArray.sort((a,b)=>b[1]-a[1]);
-    return numsArray[0][0]
+    return numsArray[0][0] */
+    let majorityNum;
+    let count = 0;
+    for(let i = 0; i<nums.length;i++){
+        if(count === 0){
+            majorityNum = nums[i]
+            count++;
+        } else {
+            majorityNum === nums[i] ? count++ : count--;
+        }
+    };
+    return majorityNum;
 };
