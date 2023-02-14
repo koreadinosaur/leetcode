67faa5913e157435e1376ebb87fd733bd4c89bb7
HJ <91491029+koreadinosaur@users.noreply.github.com>
2023-02-14 10:12:57 +0900

    Time: 92 ms (89.15%), Space: 44.1 MB (87.76%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.js
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -3,18 +3,22 @@
  * @param {number[]} s
  * @return {number}
  10시 5분
+ 이거 왜 g.sort를 오름차순으로 하면 안되지?
+ 탐욕이 높은 아이부터 정렬하고 그 친구들 먼저 충족시켜줘야하는
+ 이유가 따로 있나?
+ g 60 70 80 90 100
+ s 45 55 75 90 90
  */
 var findContentChildren = function(g, s) {
     g.sort((a,b)=>b-a);
-    s.sort();
+    s.sort((a,b)=>b-a);
     let count = 0;
+    let j = 0;
     for(let i = 0; i<g.length ; i++){
-        for(let j = 0; j<s.length; j++ ){
-            if(g[i]<= s[j]){
-                s.splice(j,1);
-                count++;
-                break;
-            }
+        if(!s[j]) break;
+        if(g[i] <= s[j]){
+            count++;
+            j++;
         }
     }
     return count
