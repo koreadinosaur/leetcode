1dc995240aabf7f4ac0d7aca104caa763dadd22d
HJ <91491029+koreadinosaur@users.noreply.github.com>
2023-02-16 09:32:38 +0900

    Time: 93 ms (42.16%), Space: 44.6 MB (30.36%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -4,16 +4,16 @@
  25분
  */
 var missingNumber = function(nums) {
-    /*nums.sort((a,b)=>a-b);
+    nums.sort((a,b)=>a-b);
     for(let i = 0; i<nums.length ; i++){
         if(i !== nums[i]){
             return i;
         }
     }
-    return nums.length;*/
-    let sum = nums.length*(nums.length +1)/2
+    return nums.length;
+   /* let sum = nums.length*(nums.length +1)/2
     for(let i =0; i<nums.length ; i++){
        sum -= nums[i] 
     }
-    return sum;
+    return sum;*/
 };
