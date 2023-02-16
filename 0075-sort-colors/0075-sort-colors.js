91daad7106600e168a8fa7e23d37044ab25ea75a
HJ <91491029+koreadinosaur@users.noreply.github.com>
2023-02-16 16:23:54 +0900

    Time: 67 ms (57.47%), Space: 42 MB (54.95%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ 45분
+ */
+var sortColors = function(nums) {
+    return nums.sort((a,b)=>a-b)
+};
