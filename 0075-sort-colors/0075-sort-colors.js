b545dcdb0440e33a6234427f25230608f93c9dfd
HJ <91491029+koreadinosaur@users.noreply.github.com>
2023-02-16 16:43:07 +0900

    Time: 60 ms (84.74%), Space: 41.9 MB (73.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.js
+++ 0075-sort-colors/0075-sort-colors.js
@@ -3,19 +3,20 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  45분
  */
-var sortColors = function(nums) {
- 
+var sortColors = function(nums) { 
     let whiteCount = 0;
     let blueCount = 0;
     for(let i = 0; i<nums.length ; i++){
         if(nums[i]===1) whiteCount++;
         if(nums[i]===2) blueCount++;
     }
-      nums.fill(0)
-    for(let i = nums.length - whiteCount - blueCount; i < nums.length-blueCount; i++){
-        nums[i] = 1;
-    }
-    for(let i =nums.length - blueCount ; i <nums.length;i++){
-        nums[i] = 2;
-    }
+      nums.fill(0, 0,nums.length-whiteCount-blueCount);
+    nums.fill(1,nums.length-whiteCount-blueCount, nums.length-blueCount);
+    nums.fill(2,nums.length-blueCount,nums.length)
+    // for(let i = nums.length - whiteCount - blueCount; i < nums.length-blueCount; i++){
+    //     nums[i] = 1;
+    // }
+    // for(let i =nums.length - blueCount ; i <nums.length;i++){
+    //     nums[i] = 2;
+    // }
 };
