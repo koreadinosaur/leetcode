b30f919e042be0ad312b2b862a34a827a9a936e8
HJ <91491029+koreadinosaur@users.noreply.github.com>
2023-02-17 09:56:11 +0900

    Time: 74 ms (48.77%), Space: 42.5 MB (49.22%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -5,7 +5,6 @@
  */
 var isValid = function(s) {
     let stack = [];
-    if(s.length % 2 === 1)return false;
     for(let i = 0; i< s.length ; i++){
         if(s[i] === '(' || s[i]==='[' || s[i]==='{'){
             stack.push(s[i]);
@@ -14,8 +13,7 @@ var isValid = function(s) {
             if(s[i] === ')' && poped !== '(') return false;
                if(s[i] ===']' && poped !== '[') return false;
             if(s[i]==='}'&&poped !== '{') return false;
-       }
-        
+       }        
     }
     if(stack.length > 0) return false;
     return true;
