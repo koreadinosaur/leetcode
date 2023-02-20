7c3e3be24df7fb23657153dcf75658b97c0383f4
HJ <91491029+koreadinosaur@users.noreply.github.com>
2023-02-20 17:11:59 +0900

    Time: 79 ms (32.30%), Space: 44.8 MB (19.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.js
+++ 0229-majority-element-ii/0229-majority-element-ii.js
@@ -4,13 +4,22 @@
  10분
  */
 var majorityElement = function(nums) {
-    let frequency = {}
-        let result = []
-    for(let ele of nums){
-        frequency[ele] = (frequency[ele]||0) +1;
-        if(frequency[ele]>nums.length/3&&result.indexOf(ele)=== -1){
-            result.push(ele)
-        }
+    // let frequency = {}
+    //     let result = []
+    // for(let ele of nums){
+    //     frequency[ele] = (frequency[ele]||0) +1;
+    //     if(frequency[ele]>nums.length/3&&result.indexOf(ele)=== -1){
+    //         result.push(ele)
+    //     }
+    // }
+    // return result
+    let map ={}
+    for(let i of nums){
+        map[i] = map[i] ? map[i] + 1 : 1
     }
-    return result
+    let res =[]
+    for(let i in map){
+        if(map[i]>nums.length/3) res.push(i)
+    }
+    return res
 };
